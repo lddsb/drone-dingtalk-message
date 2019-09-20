664c2e90c22c65cf6ac5899a364e30e603544e8a
lddsb <luodi0128@gmail.com>
2019-09-20 10:20:35 +0800

    complete tpl feature

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/joho/godotenv v1.3.0
-	github.com/lddsb/dingtalk-webhook v0.0.0-20190307231412-b4abe34b5fa9
+	github.com/lddsb/dingtalk-webhook v0.0.1
 	github.com/urfave/cli v1.20.0
 )
--- plugin_test.go
+++ plugin_test.go
@@ -12,21 +12,10 @@ func TestPlugin(t *testing.T) {
 	}
 
 	p.Config.AccessToken = "example-access-token"
-	err = p.Exec()
-	if nil == err {
-		t.Error("commit sha length error should be catch!")
-	}
-
-	p.Drone.Commit.Sha = "53729847dfksj"
+	p.Custom.Tpl = "tpls/markdown.tpl"
 	err = p.Exec()
 	if nil == err {
 		t.Error("not support message type error should be catch!")
-	}
-
-	p.Config.MsgType = "text"
-	err = p.Exec()
-	if nil == err {
-		t.Error("access token invalid error should be catch!")
 	}
 
 	p.Config.MsgType = "link"
@@ -35,20 +24,33 @@ func TestPlugin(t *testing.T) {
 		t.Error("access token invalid error should be catch!")
 	}
 
-	p.Extra.Color.WithColor = true
-	p.Extra.Color.FailureColor = "#555555"
-	p.Extra.Color.SuccessColor = "#222222"
-	p.Extra.Pic.WithPic = true
-	p.Extra.Pic.FailurePicURL = "https://www.baidu.com"
-	p.Extra.Pic.SuccessPicURL = "https://www.baidu.com"
-	p.Extra.LinkSha = true
-	// p.Drone.Build.Status = "failure"
-	p.Config.MsgType = "markdown"
+	p.Custom.Tpl = "https://aaa.com"
+	p.Config.MsgType = "text"
 	err = p.Exec()
 	if nil == err {
 		t.Error("access token invalid error should be catch!")
 	}
 
+
+	p.Custom.Tpl = ""
+	p.Config.MsgType = "link"
+	err = p.Exec()
+	if nil == err {
+		t.Error("access token invalid error should be catch!")
+	}
+
+	p.Custom.Color.FailureColor = "#555555"
+	p.Custom.Color.SuccessColor = "#222222"
+	p.Custom.Pic.FailurePicURL = "https://www.baidu.com"
+	p.Custom.Pic.SuccessPicURL = "https://www.baidu.com"
+	p.Config.MsgType = "markdown"
+	p.Custom.Tpl = "tpls/markdown.tpl"
+	err = p.Exec()
+	if nil == err {
+		t.Error("access token invalid error should be catch!")
+	}
+
+	p.Custom.Tpl = "https://gist.githubusercontent.com/lddsb/87065e73678dcf56cd222a3c2f1f32b0/raw/fce9fb28b2c8c768eb93df5598beee8c98cba610/md.tpl"
 	p.Drone.Build.Status = "failure"
 	err = p.Exec()
 	if nil == err {
